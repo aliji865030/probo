38980c40197d11fe371163bf94c8e58419004ae2
aliji865030 <abbasalichand786@gmail.com>
2024-09-14 17:42:19 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+import NavBar from './components/NavBar/NavBar'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+    <div className='nav'>
+      <div className='nav-first'>
+        <div className='nav-first-main'>
+            <div>
+                <img src="https://probo.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.b8a0a3e7.webp&w=128&q=75" alt="" />
+            </div>
+
+            <div>
+                <ul>
+                    <li>Trading</li>
+                    <li>Team 11</li>
+                    <li>Read</li>
+                    <li>Cares</li>
+                    <li>Careers</li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div>
+            <span>For 18 years and above only</span>
+            <button>Download App</button>
+            <button>Login/Signup</button>
+            <img src="https://probo.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftranslation.5ad894fb.png&w=32&q=75" alt="" />
+
+        </div>
+      </div>
+      <div className='nav-second'>
+        <ul>
+            <li>All events</li>
+            <li>Cricket</li>
+            <li>Crypto</li>
+            <li>News</li>
+            <li>Economy</li>
+            <li>Elections</li>
+            <li>Youtube</li>
+            <li>Football</li>
+            <li>Stocks</li>
+            <li>Motorsports</li>
+            <li>Tennis</li>
+            <li>Probo</li>
+            <li>Sports</li>
+        </ul>
+
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
